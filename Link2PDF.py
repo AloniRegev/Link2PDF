52371c18d36d2a61a0bf3db2c100482cd9d46ca1
Regev Aloni <54915373+AloniRegev@users.noreply.github.com>
2022-08-15 14:40:22 +0300

    V3.1
    Now check if the directory is already has URL files in it and if so convert them before the observer start listening

--- Link2PDF.py
+++ Link2PDF.py
@@ -11,6 +11,28 @@ import pywintypes
 from win10toast import ToastNotifier
 
 
+def pdfHendler():
+    folder_file = SOURCE_PATH
+    dir_list = os.listdir(folder_file)
+    for file_name in dir_list:
+        if file_name.split('.')[-1] == "url":
+            print("Start converting.")
+            input_path = path.join(folder_file, file_name)
+            try:
+                pdfThat(file_name.split(".")[0], input_path, output_path)
+            except:
+                except_orig_path=path.join(SOURCE_PATH, file_name)
+                except_dust_path=path.join(SOURCE_PATH,"Couldent Convert", file_name)
+                move(except_orig_path, except_dust_path)
+                print("Couldn't convert.")
+                toast.show_toast("Link2PDF", "Couldn't convert \"{}\" URL.".format(str(file_name.split(".")[0])))
+            
+            if os.path.isfile(input_path):
+                path_name=path.join(output_path,"URL", file_name)
+                move(input_path, path_name)
+                toast.show_toast("Link2PDF", "\"{}\" has been converted.".format(str(file_name.split(".")[0])))
+            
+            
 def pdfThat(name, input_path, output_path):
     option = {'encoding': 'UTF-8', 'enable-local-file-access': True}
     config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
@@ -29,6 +51,7 @@ def extractURL(path):
                 url = line[4:]
                 break
     return url
+    
 
 
 class OnMyWatch:
@@ -41,13 +64,14 @@ class OnMyWatch:
         event_handler = Handler()
         self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
         self.observer.start()
+        print("Now listening {} directory.".format(SOURCE_PATH.split("\\")[-1]))
         try:
             while True:
                 time.sleep(60)
         except:
-            toast.show_toast("Link2PDF", "The program has stopped")
             self.observer.stop()
             print("Observer Stopped")
+            toast.show_toast("Link2PDF", "The program has stopped")
 
         self.observer.join()
 
@@ -60,26 +84,8 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'created' or event.event_type == 'modified':
             # Event is created, you can process it now
-            print("starting converting.")
-            folder_file = SOURCE_PATH
-            dir_list = os.listdir(folder_file)
-            for file_name in dir_list:
-                if file_name.split('.')[-1] != "url":
-                    continue
-                input_path = path.join(folder_file, file_name)
-                try:
-                    pdfThat(file_name.split(".")[0], input_path, output_path)
-                except:
-                    except_orig_path=path.join(SOURCE_PATH, file_name)
-                    except_dust_path=path.join(SOURCE_PATH,"Couldent Convert", file_name)
-                    move(except_orig_path, except_dust_path)
-                    print("couldent convert.")
-                    toast.show_toast("Link2PDF", "couldent convert \"{}\" URL.".format(str(file_name.split(".")[0])))
-                
-                if os.path.isfile(input_path):
-                    path_name=path.join(output_path,"URL", file_name)
-                    move(input_path, path_name)
-                    toast.show_toast("Link2PDF", "\"{}\" has converted.".format(str(file_name.split(".")[0])))
+            pdfHendler()
+            
         elif event.event_type == 'deleted':
             print("File has deleted!")
 
@@ -98,6 +104,12 @@ if __name__ == '__main__':
         os.makedirs(path.join(output_path, "PDF"))
     if not os.path.exists(path.join(SOURCE_PATH, "Couldent Convert")):
         os.makedirs(path.join(SOURCE_PATH, "Couldent Convert"))
-        
+    
+    if len(os.listdir(path.join(SOURCE_PATH, "Couldent Convert"))) != 0:
+        print("directory is not empty, your attention is required")
+        toast.show_toast("Link2PDF", "You have few URLs that couldent convert, your attention is required.")
+    
+    pdfHendler()
+    
     watch = OnMyWatch(SOURCE_PATH)
     watch.run()
